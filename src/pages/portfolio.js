4e1ccf5e5643e53c3b8c27dfe7f413d4c2b1715c
aJesusCode <ajesusnode@gmail.com>
2023-07-03 13:21:18 +0200

    new pics added

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -49,7 +49,7 @@ export default function Portfolio({ images }) {
                                         width="800"
                                         height="800"
                                         src={image.title}
-                                        sizes="100vw"
+                                        sizes="25vw"
                                         alt={image.title}
                                     />
                                 </div>
@@ -95,7 +95,7 @@ export default function Portfolio({ images }) {
                             className="w-[64rem] object-contain"
                             width="1920"
                             height="1920"
-                            sizes="100vw"
+                            sizes="75vw"
                         />
                     </div>
                 </div>
