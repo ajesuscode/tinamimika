a6341355f14fedb3c8a52039dfcbd0035d145d97
aJesusCode <ajesusnode@gmail.com>
2023-07-03 23:22:59 +0200

    orientation lock

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,9 +6,9 @@ export default function About() {
                 <span className="font-display text-lg uppercase tracking-widest lg:text-3xl">
                     Tina Mimika
                 </span>
-                , and I'm passionate about capturing the beauty of interior
-                spaces, architecture, landscapes, and the ocean through my
-                medium format photography.
+                and I'm passionate about capturing the beauty of interior
+                spaces, landscapes, and the ocean through my medium format
+                photography.
             </p>
             <p className="font-body  font-normal text-zinc-200 pt-4">
                 Over the years, I've honed my skills and developed a keen eye
@@ -18,16 +18,18 @@ export default function About() {
                 projects that showcase the unique beauty of each subject.
             </p>
             <p className="font-body  font-normal text-zinc-200 pt-4">
-                Photography has been a passion of mine since I was young. I
-                spent hours exploring the world around me with a camera in hand,
-                and over time, my love for capturing the essence of each subject
-                grew into a career.
+                My photography love for capturing the essence of each subject
+                grew into a career. With extensive experience as a creative
+                director in advertising, I possess the skillset to artistically
+                connect clients, customers, and their desired products or
+                services. I bring a unique blend of creativity and business
+                acumen to deliver exceptional results.
             </p>
             <p className="font-body  font-normal text-zinc-200 pt-4">
                 Aside from photography, I'm also deeply passionate about the
                 environment and the natural world. I've always had a love for
-                the ocean, and this passion is reflected in my breathtaking
-                seascapes, which capture the power and majesty of the sea.
+                the ocean, and this passion is reflected in my oceanscapes,
+                which capture the power and majesty of the Father Ocean.
             </p>
         </section>
     );
